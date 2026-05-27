1481e8b5c7bab76e6f65897690c68e7eaa9c28c5
Lazzat Seiilova <lseiilova@gmail.com>
2026-05-27 10:16:54 +0500

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Lazzat Seiilova on 27.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
