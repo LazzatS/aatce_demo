ae3472354604555876ea32e94f37f6a91b330160
sturdytea <aitmazh@gmail.com>
2026-05-29 11:58:24 +0500

    fix: add app icon

--- DemoApp/DemoAppApp.swift
+++ DemoApp/DemoAppApp.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 @main
 struct DemoAppApp: App {
+    @State private var isLaunching = true
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if isLaunching {
+                SplashView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            isLaunching = false
+                        }
+                    }
+            } else {
+                ContentView()
+            }
+        }
+    }
+}
+
+struct SplashView: View {
+    var body: some View {
+        VStack {
+            Image("AppLogo")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 30))
         }
     }
 }
